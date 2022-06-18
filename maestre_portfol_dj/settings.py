31aaad3691aea436e0a98c1a303fccae68141578
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2022-06-18 19:30:55 +0200

    Configurar host

--- maestre_portfol_dj/settings.py
+++ maestre_portfol_dj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2u961yv@p9-$8%$n&g%cwn+&(@1rf4u=n8&923mr3qtogj!hge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.180.31.120']
 
 
 # Application definition
